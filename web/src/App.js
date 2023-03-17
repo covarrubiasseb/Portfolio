14bc2e29531d1c0bbd688f1ef0391430a4fbda92
unknown <covarrubias.seb@gmail.com>
2023-03-16 22:22:04 -0700

    Added Portfolio cards and images.

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -57,54 +57,58 @@ function App() {
                     <div className="divider-custom-line"></div>
                 </div>
                 <div className="row justify-content-center">
+
                     <div className="col-md-6 col-lg-4 mb-5">
                         <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
                             <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                 <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img className="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." />
+                            <div className="card shadow bg-transparent border-0 text-white">
+                                <img className="img-fluid" src="assets/img/portfolio/InProgress.jpg" alt="..." style={{width: "480px", height: "296px"}} />
+                                <div className="card-body">
+                                    <h5 className="card-title">In Progress</h5>
+                                    <p className="card-text">
+                                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
                     <div className="col-md-6 col-lg-4 mb-5">
                         <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
                             <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                 <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img className="img-fluid" src="assets/img/portfolio/cake.png" alt="..." />
+                            <div className="card shadow bg-transparent border-0 text-white">
+                                <img className="img-fluid" src="assets/img/portfolio/TicketDesk.png" alt="..." style={{width: "480px", height: "296px"}} />
+                                <div className="card-body">
+                                    <h5 className="card-title">Ticket Desk</h5>
+                                    <p className="card-text">
+                                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
                     <div className="col-md-6 col-lg-4 mb-5">
                         <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
                             <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                 <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img className="img-fluid" src="assets/img/portfolio/circus.png" alt="..." />
-                        </div>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-5 mb-lg-0">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            <div className="card shadow bg-transparent border-0 text-white">
+                                <img className="img-fluid" src="assets/img/portfolio/SSF2X.png" alt="..."style={{width: "480px", height: "296px"}}  />
+                                <div className="card-body">
+                                    <h5 className="card-title">SSF2X Rankings</h5>
+                                    <p className="card-text">
+                                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                                    </p>
+                                </div>
                             </div>
-                            <img className="img-fluid" src="assets/img/portfolio/game.png" alt="..." />
                         </div>
                     </div>
-                    <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src="assets/img/portfolio/safe.png" alt="..." />
-                        </div>
-                    </div>
-                    <div className="col-md-6 col-lg-4">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal6">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src="assets/img/portfolio/submarine.png" alt="..." />
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </section>
@@ -121,13 +125,13 @@ function App() {
                         <div className="container">
                             <div className="row justify-content-center">
                                 <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Log Cabin</h2>
+                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">In Progress</h2>
                                     <div className="divider-custom">
                                         <div className="divider-custom-line"></div>
                                         <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
                                         <div className="divider-custom-line"></div>
                                     </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/cabin.png" alt="..." />
+                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/InProgress.jpg" alt="..." />
                                     <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
                                     <button className="btn btn-primary" data-bs-dismiss="modal">
                                         <i className="fas fa-xmark fa-fw"></i>
@@ -149,13 +153,13 @@ function App() {
                         <div className="container">
                             <div className="row justify-content-center">
                                 <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Tasty Cake</h2>
+                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Ticket Desk</h2>
                                     <div className="divider-custom">
                                         <div className="divider-custom-line"></div>
                                         <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
                                         <div className="divider-custom-line"></div>
                                     </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/cake.png" alt="..." />
+                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/TicketDesk.png" alt="..." />
                                     <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
                                     <button className="btn btn-primary" data-bs-dismiss="modal">
                                         <i className="fas fa-xmark fa-fw"></i>
@@ -177,97 +181,13 @@ function App() {
                         <div className="container">
                             <div className="row justify-content-center">
                                 <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Circus Tent</h2>
+                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">SSF2X Rankings</h2>
                                     <div className="divider-custom">
                                         <div className="divider-custom-line"></div>
                                         <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
                                         <div className="divider-custom-line"></div>
                                     </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/circus.png" alt="..." />
-                                    <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                    <button className="btn btn-primary" data-bs-dismiss="modal">
-                                        <i className="fas fa-xmark fa-fw"></i>
-                                        Close Window
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className="portfolio-modal modal fade" id="portfolioModal4" tabIndex="-1" aria-labelledby="portfolioModal4" aria-hidden="true">
-            <div className="modal-dialog modal-xl">
-                <div className="modal-content">
-                    <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
-                    <div className="modal-body text-center pb-5">
-                        <div className="container">
-                            <div className="row justify-content-center">
-                                <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Controller</h2>
-                                    <div className="divider-custom">
-                                        <div className="divider-custom-line"></div>
-                                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                        <div className="divider-custom-line"></div>
-                                    </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/game.png" alt="..." />
-                                    <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                    <button className="btn btn-primary" data-bs-dismiss="modal">
-                                        <i className="fas fa-xmark fa-fw"></i>
-                                        Close Window
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className="portfolio-modal modal fade" id="portfolioModal5" tabIndex="-1" aria-labelledby="portfolioModal5" aria-hidden="true">
-            <div className="modal-dialog modal-xl">
-                <div className="modal-content">
-                    <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
-                    <div className="modal-body text-center pb-5">
-                        <div className="container">
-                            <div className="row justify-content-center">
-                                <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Locked Safe</h2>
-                                    <div className="divider-custom">
-                                        <div className="divider-custom-line"></div>
-                                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                        <div className="divider-custom-line"></div>
-                                    </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/safe.png" alt="..." />
-                                    <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                    <button className="btn btn-primary" data-bs-dismiss="modal">
-                                        <i className="fas fa-xmark fa-fw"></i>
-                                        Close Window
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className="portfolio-modal modal fade" id="portfolioModal6" tabIndex="-1" aria-labelledby="portfolioModal6" aria-hidden="true">
-            <div className="modal-dialog modal-xl">
-                <div className="modal-content">
-                    <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
-                    <div className="modal-body text-center pb-5">
-                        <div className="container">
-                            <div className="row justify-content-center">
-                                <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Submarine</h2>
-                                    <div className="divider-custom">
-                                        <div className="divider-custom-line"></div>
-                                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                        <div className="divider-custom-line"></div>
-                                    </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/submarine.png" alt="..." />
+                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/SSF2X.png" alt="..." />
                                     <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
                                     <button className="btn btn-primary" data-bs-dismiss="modal">
                                         <i className="fas fa-xmark fa-fw"></i>
