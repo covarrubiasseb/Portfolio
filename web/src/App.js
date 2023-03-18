7748f285d8f6899ce71e54ce0a3005b31327760a
unknown <covarrubias.seb@gmail.com>
2023-03-17 18:51:14 -0700

    Restyled Project modals to dark theme.

--- web/src/App.js
+++ web/src/App.js
@@ -89,7 +89,7 @@ function App() {
                                 <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                             </div>
                             <div className="card shadow bg-transparent border-0 text-white">
-                                <img className="img-fluid" src="assets/img/portfolio/SSF2X.png" alt="..."style={{width: "480px", height: "296px"}}  />
+                                <img className="img-fluid" src="assets/img/portfolio/SSF2X.png" alt="..." style={{width: "480px", height: "296px"}}  />
                                 <div className="card-body text-center">
                                     <h5 className="card-title">SSF2X Rankings</h5>
                                     <p className="card-text">
@@ -110,20 +110,16 @@ function App() {
 
         <div className="portfolio-modal modal fade" id="portfolioModal1" tabIndex="-1" aria-labelledby="portfolioModal1" aria-hidden="true">
             <div className="modal-dialog modal-xl">
-                <div className="modal-content">
+                <div className="modal-content bg-secondary bg-gradient">
                     <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
                     <div className="modal-body text-center pb-5">
                         <div className="container">
                             <div className="row justify-content-center">
                                 <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">In Progress</h2>
-                                    <div className="divider-custom">
-                                        <div className="divider-custom-line"></div>
-                                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                        <div className="divider-custom-line"></div>
-                                    </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/InProgress.jpg" alt="..." />
-                                    <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                    <h2 className="portfolio-modal-title text-primary text-uppercase mb-0 pb-4">In Progress</h2>
+                                    
+                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/InProgress.jpg" alt="..." style={{width: "640px", height: "480px"}} />
+                                    <p className="mb-4 text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
                                     <button className="btn btn-primary" data-bs-dismiss="modal">
                                         <i className="fas fa-xmark fa-fw"></i>
                                         Close Window
@@ -138,20 +134,16 @@ function App() {
 
         <div className="portfolio-modal modal fade" id="portfolioModal2" tabIndex="-1" aria-labelledby="portfolioModal2" aria-hidden="true">
             <div className="modal-dialog modal-xl">
-                <div className="modal-content">
+                <div className="modal-content bg-secondary bg-gradient">
                     <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
                     <div className="modal-body text-center pb-5">
                         <div className="container">
                             <div className="row justify-content-center">
                                 <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Ticket Desk</h2>
-                                    <div className="divider-custom">
-                                        <div className="divider-custom-line"></div>
-                                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                        <div className="divider-custom-line"></div>
-                                    </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/TicketDesk.png" alt="..." />
-                                    <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                    <h2 className="portfolio-modal-title text-primary text-uppercase mb-0 pb-4">Ticket Desk</h2>
+                                    
+                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/TicketDesk.png" alt="..." style={{width: "640px", height: "480px"}} />
+                                    <p className="mb-4 text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
                                     <button className="btn btn-primary" data-bs-dismiss="modal">
                                         <i className="fas fa-xmark fa-fw"></i>
                                         Close Window
@@ -166,20 +158,16 @@ function App() {
 
         <div className="portfolio-modal modal fade" id="portfolioModal3" tabIndex="-1" aria-labelledby="portfolioModal3" aria-hidden="true">
             <div className="modal-dialog modal-xl">
-                <div className="modal-content">
+                <div className="modal-content bg-secondary bg-gradient">
                     <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
                     <div className="modal-body text-center pb-5">
                         <div className="container">
                             <div className="row justify-content-center">
                                 <div className="col-lg-8">
-                                    <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">SSF2X Rankings</h2>
-                                    <div className="divider-custom">
-                                        <div className="divider-custom-line"></div>
-                                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                                        <div className="divider-custom-line"></div>
-                                    </div>
-                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/SSF2X.png" alt="..." />
-                                    <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                    <h2 className="portfolio-modal-title text-primary text-uppercase mb-0 pb-4">SSF2X Rankings</h2>
+                                    
+                                    <img className="img-fluid rounded mb-5" src="assets/img/portfolio/SSF2X.png" alt="..." style={{width: "640px", height: "480px"}} />
+                                    <p className="mb-4 text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
                                     <button className="btn btn-primary" data-bs-dismiss="modal">
                                         <i className="fas fa-xmark fa-fw"></i>
                                         Close Window
